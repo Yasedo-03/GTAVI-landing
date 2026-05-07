b90a1519c1daa6bf7b687bd3e7520c60fb3d9f43
Adrien LE BORGNE <adrienleborgne@MacBook-Pro-de-Adrien-2.local>
2026-05-07 22:56:26 +0200

    Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import Hero from './sections/Hero';
+import NavBar from './sections/NavBar';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-3xl text-indigo-500">Welcome</h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
