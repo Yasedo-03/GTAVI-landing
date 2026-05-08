da6a6adaabdf77aa367a0a149a406a14b20a8e71
Adrien LE BORGNE <adrienleborgne@MacBook-Pro-de-Adrien-2.local>
2026-05-08 17:15:22 +0200

    implement jason section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
-import Hero from './sections/Hero';
+
 import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
 import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,6 +14,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <FirstVideo />
+      <Jason />
     </main>
   );
 };
